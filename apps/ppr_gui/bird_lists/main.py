499d49f7b87496b7bc734f3af7dc31bd28fa6c10
Louis Pi <granbobbi22@gmail.com>
2019-02-28 19:44:51 +0100

    updates and not working new app

--- apps/ppr_gui/bird_lists/main.py
+++ apps/ppr_gui/bird_lists/main.py
@@ -1,18 +1,18 @@
-from csv import reader, writer, QUOTE_MINIMAL
-import difflib
-import os.path
-import os
+from csv import reader, writer, QUOTE_MINIMAL # Imports csv library to allow the program to read and write to the list which are saved as csv files
+import difflib # To find close matches in the search
+import os.path # To check if file already exists before creating new list
+import os # Uses the listdir function to find lists in the bird_lists folder
 
-from helpers import setup_logger
+from helpers import setup_logger # Logger for ZPUI
 
 menu_name = "Bird Lists"  # App name as seen in main menu while using the system
 
-from subprocess import call
-from time import sleep
+from subprocess import call # Uses for ZPUI logging
+from time import sleep # Used to pause the program for an amount of seconds
 
-from ui import Menu, Printer, PrettyPrinter, Listbox, DialogBox
+from ui import Menu, Printer, PrettyPrinter, Listbox, DialogBox # Import the UI functions we need from ZPUI
 
-logger = setup_logger(__name__, "info")
+logger = setup_logger(__name__, "info") # Sets up the ZPUI logger
 
 def call_internal():
         Printer(["Calling internal", "command"], i, o, 1)
@@ -21,6 +21,8 @@ def call_internal():
 def call_external():
         Printer(["Calling external", "command"], i, o, 1)
         call(["echo", "Success"])
+
+# Function to load lists from the csv files into nested lists within a dictionary
 
 def load_into_dict(d):
 	select_list()
@@ -33,6 +35,8 @@ def load_into_dict(d):
 			if row:
 				d[name].append(row)
 
+# Allows the user to select a list from the bird_lists directory
+
 def select_list():
 	files = []
 	list_names = []
@@ -43,10 +47,12 @@ def select_list():
 		file_name = file_name.replace("_", " ")
                 file_name = file_name[:-4].title()
 		list_names.append(file_name)
-	selected_list = Listbox(list_names, i, o, "List selection").activate()
+	selected_list = Listbox(list_names, i, o).activate()
 	file_number = list_names.index(selected_list)
 	global selected_file
-	selected_file = files[file_number]
+	selected_file = files[file_number] # Stores the selected list's file name to be used elsewhere
+
+# Simply reads the selected list
 
 def read_list():
 	with open("/home/pi/piportablerecorder/apps/ppr_gui/bird_lists/" + selected_file, "r") as csvfile:
@@ -54,31 +60,49 @@ def read_list():
 		for row in csv_reader:
 			print(', '.join(row))
 
+# Searches the selected list by bird, date or location and finds the closest match if there isn't a exact one when possible
+
 def search_list():
-        target = raw_input("What bird are you looking for? ").lower() # Need to add the option to do same for date and location
-	data_values = data.values()
-	match = False
-	for value in data_values:
-		for list_item in value:
- 			if match:
- 				continue
-			if list_item[0].lower() == target:
-				PrettyPrinter("{} on {} at {}".format(list_item[0], str(list_item[1]), list_item[2]), i, o, 5, None)
-				match = True
-	if not match:
-		birds = [list_item[0] for value in data_values for list_item in value]
-		dates = [list_item[1] for value in data_values for list_item in value]
-		locations = [list_item[2] for value in data_values for list_item in value]
-		best_match = difflib.get_close_matches(target, birds, 1)
-		if best_match:
-			yes_no = DialogBox("yn", i, o, message = "Did you mean {}".format(best_match[0])).activate()
-			if yes_no:
-				match_index = birds.index(best_match[0])
-				PrettyPrinter("{} on {} at {}".format(best_match[0], str(dates[match_index]), str(locations[match_index])), i, o, 5, None)
+	target_type_contents = [
+	["Bird", 0],
+	["Date", 1],
+	["Location", 2]]
+	target_type = Listbox(target_type_contents, i, o).activate()
+	if target_type in (0, 1, 2):
+		if target_type == 0:
+			target = raw_input("Bird: ").lower()
+		elif target_type == 1:
+                        target = raw_input("Date: ").lower()
+              	else:
+                        target = raw_input("Location: ").lower()
+		data_values = data.values()
+		matches = []
+		match = False
+		for value in data_values: # Need to add option to choose just one list
+			for list_item in value:
+				if list_item[target_type].lower() == target:
+					matches.append("{} on {} at {}".format(str(list_item[0]), str(list_item[1]), str(list_item[2])))
+					match = True
+		if match:
+			if len(matches) > 1:
+				PrettyPrinter("{} and {}".format(", ".join(matches[:-1]), matches[-1]), i, o, 5, None)
+			else:
+				print(matches[0])
+		if not match:
+			birds = [list_item[0] for value in data_values for list_item in value]
+			dates = [list_item[1] for value in data_values for list_item in value]
+			locations = [list_item[2] for value in data_values for list_item in value]
+			best_match = difflib.get_close_matches(target, birds, 1)
+			if best_match:
+				yes_no = DialogBox("yn", i, o, message = "Did you mean {}?".format(best_match[0])).activate()
+				if yes_no:
+					match_index = birds.index(best_match[0])
+					PrettyPrinter("{} on {} at {}".format(best_match[0], str(dates[match_index]), str(locations[match_index])), i, o, 5, None)
+				else:
+					PrettyPrinter("No match found", i, o, 5, None)
 			else:
 				PrettyPrinter("No match found", i, o, 5, None)
-		else:
-			PrettyPrinter("No match found", i, o, 5, None)
+# Adds a record to the selected list
 
 def add_to_list():
 	with open("/home/pi/piportablerecorder/apps/ppr_gui/bird_lists/" + selected_file, "a") as csvfile:
@@ -87,6 +111,8 @@ def add_to_list():
         	a_location = raw_input("Location to add: ")
 		csv_writer = writer(csvfile, delimiter=",", quotechar='"', quoting=QUOTE_MINIMAL)
 		csv_writer.writerow([a_bird, a_date, a_location])
+
+# Creates a new list
 
 def create_list():
 	file_name = raw_input("What would you like your list to be called? ").lower()
@@ -117,4 +143,3 @@ def callback():
 	["Create new list", create_list]
 	]
 	Menu(menu_contents, i, o, "Lister").activate()
-
