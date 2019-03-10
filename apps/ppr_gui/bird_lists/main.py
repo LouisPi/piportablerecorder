c3f0c0dad48712be9666f9ad34eb15abafce6be4
Louis Pi <granbobbi22@gmail.com>
2019-03-10 13:32:14 +0100

    app update

--- apps/ppr_gui/bird_lists/main.py
+++ apps/ppr_gui/bird_lists/main.py
@@ -1,16 +1,14 @@
 from csv import reader, writer, QUOTE_MINIMAL # Imports csv library to allow the program to read and write to the list which are saved as csv files
-import difflib # To find close matches in the search
-import os.path # To check if file already exists before creating new list
-import os # Uses the listdir function to find lists in the bird_lists folder
+from difflib import get_close_matches # To find close matches in the search
+from os.path import exists # To check if file already exists before creating new list
+from os import listdir # Used to find lists in the bird_lists folder
 
 from helpers import setup_logger # Logger for ZPUI
 
 menu_name = "Bird Lists"  # App name as seen in main menu while using the system
 
 from subprocess import call # Uses for ZPUI logging
-from time import sleep # Used to pause the program for an amount of seconds
-
-from ui import Menu, Printer, PrettyPrinter, Listbox, DialogBox # Import the UI functions we need from ZPUI
+from ui import Menu, Printer, PrettyPrinter, Listbox, DialogBox, Checkbox # Import the UI functions we need from ZPUI
 
 logger = setup_logger(__name__, "info") # Sets up the ZPUI logger
 
@@ -24,38 +22,55 @@ def call_external():
 
 # Function to load lists from the csv files into nested lists within a dictionary
 
-def load_into_dict(d):
-	select_list()
-	global csvfile
-	with open("/home/pi/piportablerecorder/apps/ppr_gui/bird_lists/" + selected_file) as csvfile:
-		readCSV = reader(csvfile, delimiter = ',', skipinitialspace = True)
-		name = selected_file.split(".")[0]
-		d[name] = []
-		for row in readCSV:
-			if row:
-				d[name].append(row)
+def load_into_dict(d, list):
+	for list_name in list:
+		with open("/home/pi/piportablerecorder/apps/ppr_gui/bird_lists/" + list_name) as csvfile:
+			readCSV = reader(csvfile, delimiter = ',', skipinitialspace = True)
+			name = list_name.split(".")[0]
+			d[name] = []
+			for row in readCSV:
+				if row:
+					d[name].append(row)
 
 # Allows the user to select a list from the bird_lists directory
 
 def select_list():
 	files = []
 	list_names = []
-        for file in os.listdir("/home/pi/piportablerecorder/apps/ppr_gui/bird_lists"):
+	selected_files = []
+        for file in listdir("/home/pi/piportablerecorder/apps/ppr_gui/bird_lists"):
 		if file.endswith(".csv"):
 			files.append(file)
 	for file_name in files:
 		file_name = file_name.replace("_", " ")
                 file_name = file_name[:-4].title()
 		list_names.append(file_name)
-	selected_list = Listbox(list_names, i, o).activate()
-	file_number = list_names.index(selected_list)
-	global selected_file
-	selected_file = files[file_number] # Stores the selected list's file name to be used elsewhere
+	list_names_and_files = list(zip(list_names, files))
+	lbc = [
+	["Single select", "single"],
+	["Multiple select", "multi"],
+	["Select all", "all"],
+	]
+	amount_of_files = Listbox(lbc, i, o).activate()
+	if amount_of_files == "single":
+		selected_list = Listbox(list_names_and_files, i, o).activate()
+		selected_files.append(selected_list)
+		#file_number = list_names.index(selected_list)
+		# global selected_file
+		#selected_files.append(files[file_number]) # Stores the selected list's file name to be used elsewhere
+	elif amount_of_files == "multi":
+		multi_files = Checkbox(list_names_and_files, i, o).activate()
+		selected_files = [key for key, value in multi_files.items() if value]
+	elif amount_of_files == "all":
+		selected_files = files
+	else:
+		pass
+	return selected_files
 
 # Simply reads the selected list
 
 def read_list():
-	with open("/home/pi/piportablerecorder/apps/ppr_gui/bird_lists/" + selected_file, "r") as csvfile:
+	with open("/home/pi/piportablerecorder/apps/ppr_gui/bird_lists/" + select_list(), "r") as csvfile:
 		csv_reader = reader(csvfile, delimiter=",", quotechar='"')
 		for row in csv_reader:
 			print(', '.join(row))
@@ -63,6 +78,8 @@ def read_list():
 # Searches the selected list by bird, date or location and finds the closest match if there isn't a exact one when possible
 
 def search_list():
+	selected_files = select_list()
+	load_into_dict(data, selected_files)
 	target_type_contents = [
 	["Bird", 0],
 	["Date", 1],
@@ -75,7 +92,10 @@ def search_list():
                         target = raw_input("Date: ").lower()
               	else:
                         target = raw_input("Location: ").lower()
-		data_values = data.values()
+		search_keys = []
+		for file_name in selected_files:
+			search_keys.append(file_name.split(".")[0])
+		data_values = [data[x] for x in search_keys]
 		matches = []
 		match = False
 		for value in data_values: # Need to add option to choose just one list
@@ -87,12 +107,12 @@ def search_list():
 			if len(matches) > 1:
 				PrettyPrinter("{} and {}".format(", ".join(matches[:-1]), matches[-1]), i, o, 5, None)
 			else:
-				print(matches[0])
+				PrettyPrinter(matches[0], i, o, 5, None)
 		if not match:
 			birds = [list_item[0] for value in data_values for list_item in value]
 			dates = [list_item[1] for value in data_values for list_item in value]
 			locations = [list_item[2] for value in data_values for list_item in value]
-			best_match = difflib.get_close_matches(target, birds, 1)
+			best_match = get_close_matches(target, birds, 1)
 			if best_match:
 				yes_no = DialogBox("yn", i, o, message = "Did you mean {}?".format(best_match[0])).activate()
 				if yes_no:
@@ -105,7 +125,7 @@ def search_list():
 # Adds a record to the selected list
 
 def add_to_list():
-	with open("/home/pi/piportablerecorder/apps/ppr_gui/bird_lists/" + selected_file, "a") as csvfile:
+	with open("/home/pi/piportablerecorder/apps/ppr_gui/bird_lists/" + select_list(), "a") as csvfile:
 		a_bird = raw_input("Bird to add: ")
 		a_date = raw_input("Date to add: ")
         	a_location = raw_input("Location to add: ")
@@ -135,7 +155,7 @@ def init_app(input, output):
 def callback():
 	global data
 	data = {}
-	load_into_dict(data)
+	# load_into_dict(data)
 	menu_contents = [
 	["Read", read_list],
 	["Search", search_list],
